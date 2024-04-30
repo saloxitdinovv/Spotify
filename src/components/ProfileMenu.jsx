2a09e1b441eeaaf53b7e24602b94528f3e4c0da0
saloxitdinovv <134138858+saloxitdinovv@users.noreply.github.com>
2024-04-30 23:53:01 +0500

    changes added

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -1,18 +1,47 @@
+import { useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { LuExternalLink } from "react-icons/lu";
 
 
+function ProfileMenu() {
+    const [isOpen, setIsOpen] = useState(false)
 
-function ProfileMenu(params) {
+    function logOut() {
+        localStorage.removeItem('token')
+        location.reload()
+    }
 
 
     return (
-        <div className="cursor-pointer flex items-center gap-2 bg-[#0A0A0A] rounded-3xl p-0.5 text-white select-none">
-            <img className="rounded-full object-cover" src="/images/user.png" alt="" />
-            <span>User</span>
-            <button>
-                <IoMdArrowDropdown size={24} />
-            </button>
-        </div>
+        <>
+            <div onClick={() => setIsOpen(!isOpen)}
+                className="cursor-pointer flex items-center gap-2 bg-[#0A0A0A] rounded-3xl p-0.5 text-white select-none"
+            >
+                <img className="rounded-full object-cover" src="/images/user.png" alt="" />
+                <span>User</span>
+                <button>
+                    <IoMdArrowDropdown size={24} />
+                </button>
+            </div>
+            {
+                isOpen ? (
+                    <div className="w-60 p-5 bg-[#282828] absolute right-10 top-20 rounded-md">
+                        <ul className="text-white text-xl font-medium flex flex-col gap-4">
+                            <li className="flex justify-between items-center cursor-pointer">
+                                <span>Account</span>
+                                <LuExternalLink size={25} />
+                            </li>
+                            <li className="cursor-pointer">Profile</li>
+                            <li 
+                                onClick={() => logOut()}
+                                className="cursor-pointer"
+                            >Log out</li>
+                        </ul>
+                    </div>
+                ) : null
+            }
+        </>
+
     )
 }
 
--- src/pages/Playlist_page.jsx
+++ src/pages/Playlist_page.jsx
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react"
-
+import { FaHeart } from "react-icons/fa";
 
 export default function PLaylist_page() {
     const [tracks, setTracks] = useState([])
     const [playlist, setPLaylist] = useState([])
 
+    function formatDuration(duration_ms) {
+        const minutes = Math.floor(duration_ms / 60000);
+        const seconds = ((duration_ms % 60000) / 1000).toFixed(0);
+
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    }
+
+    function formatDate(added_at) {
+        const date = new Date(added_at);
+        const formattedDate = `${date.toLocaleDateString()}`;
+
+        return formattedDate;
+    }
+
     useEffect(() => {
         const id = location.pathname.split('/').at(-1)
         const token = localStorage.getItem('token')
+
+        fetch(`${import.meta.env.VITE_PUBLIC_URL}/playlists/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(res => res.json())
+            .then(res => {
+                setPLaylist(res)
+            })
 
         fetch(`${import.meta.env.VITE_PUBLIC_URL}/playlists/${id}/tracks`, {
             headers: {
@@ -19,29 +43,53 @@ export default function PLaylist_page() {
                 setTracks(res.items)
             })
 
-        fetch(`${import.meta.env.VITE_PUBLIC_URL}/playlists/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-            .then(res => res.json())
-            .then(res => {
-                setPLaylist(res)
-                console.log(res);
-            })
-
     }, [])
+
+    console.log(playlist);
 
     return (
         <>
-            <div className="playlist_head">
-            <img className="w-[300px] h-[300px] object-cover" src={playlist.images[0].url} alt="" />
+            <div className="playlist_head text-white uppercase pb-[30px]">
+                {
+                    // <img className="w-[300px] h-[300px] object-cover" src={playlist.images[0].url} alt="" />
+                }
+
+                <div className="text text-left">
+                    <h1 className="type text-lg">{playlist.type}</h1>
+
+                    <h1 className="playlist_name text-8xl">{playlist.name}</h1>
+
+                    <h1 className="descriptions">{playlist.description}</h1>
+                </div>
             </div>
 
-            <ul>
-                {tracks.map(item => (
-                    <li className="text-white flex justify-between w-[600px] items-center pb-5" key={item.track.id}>{item.track.name}
-                        <audio src={item.track.preview_url} controls></audio>
+            <ul className="flex flex-col gap-5">
+                {tracks.map((item, index) => (
+                    <li className="text-white flex items-center cursor-pointer" key={item.track.id}>
+                        <span className="number text-right pr-5 w-10">{index + 1}</span>
+                        <div className="song_name_info flex gap-5 w-[400px]">
+                            <img src={item.track.album.images.at(-1).url} className="w-[50px]" alt="" />
+                            <div className="info">
+                                <div className="song_name text-white text-base">
+                                    {item.track.name}
+                                </div>
+                                <div className="artists text-[#B3B3B3] text-sm">
+                                    {item.track.artists[0].name}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="album_info text-sm text-[#B3B3B3] text-left w-[300px]">{item.track.album.name}</div>
+                        <div className="data w-[200px]">
+                            {formatDate(item.added_at)}
+                        </div>
+                        <div className="last_box flex items-center justify-end w-[190px] gap-8">
+                            <button className="liked"><FaHeart color="#63CF6C" size={20} /></button>
+                            <span className="duration">
+                                {
+                                    formatDuration(item.track.duration_ms)
+                                }
+                            </span>
+                        </div>
                     </li>
                 ))}
             </ul>
